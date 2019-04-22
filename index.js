088204d32eb86de6acc1cc72b3f522dba2de46d8
gamontec <gamontec@uc.cl>
2019-04-21 20:11:14 -0400

    setting heroku db

--- index.js
+++ index.js
@@ -5,30 +5,35 @@ const path = require('path')
 const mustacheExpress = require('mustache-express');
 const { Client } = require('pg')
 
+let client
+if(process.env.DATABASE_URL){
+    client = new Client({connectionString: process.env.DATABASE_URL, ssl: true})
+}
+else{
+    client = new Client({database: 'postgresql-asymmetrical-57151'})
+}
+
+client.connect()
+
 let app = express()
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
-
-let client = new Client({database: 'dbtest1'})
-client.connect()
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
-    // client.query('SELECT * FROM posts', (err, resSQL) => {
-    //     if (err) {
-    //       console.log(err.stack)
-    //     } else {
-    //         let someArray = resSQL.rows
-    //         res.render('index', {
-    //             someArray
-    //         });
-    //     }
-    //   })
-    res.send("hello world")
+    client.query('SELECT * FROM posts', (err, resSQL) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+            let someArray = resSQL.rows
+            res.render('index', {
+                someArray
+            });
+        }
+      })
 })
 
 app.post('/post', function(req, res){
